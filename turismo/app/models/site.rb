52cb65d35a2acc4aa48c39bf8194def794f46935
Leydi <leidyflor2007@hotmail.com>
2016-03-30 17:29:32 -0500

    Instalacion gema devise

--- turismo/app/models/site.rb
+++ turismo/app/models/site.rb
@@ -1,2 +1,5 @@
 class Site < ActiveRecord::Base
+	has_many :photos
+	belongs_to :category
+	has_many :comments
 end
--- turismo/config/routes.rb
+++ turismo/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :photos
+  resources :comments
+  resources :categories
+  resources :sites
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
